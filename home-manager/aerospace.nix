fe9f71189aab62566e886ff885f8b9615799552a
sawadashota <shota@sslife.tech>
2024-08-27 20:33:57 +0900

    fix: aerospace config

--- home-manager/aerospace.nix
+++ home-manager/aerospace.nix
@@ -4,57 +4,83 @@
     text = ''
       enable-normalization-flatten-containers = false
       enable-normalization-opposite-orientation-for-nested-containers = false
+      start-at-login = true
 
       [gaps]
       inner.horizontal = 0
-      inner.vertical   = 0
-      outer.left       = 0
-      outer.bottom     = 0
-      outer.top        = 0
-      outer.right      = 0
+      inner.vertical = 0
+      outer.bottom = 0
+      outer.left = 0
+      outer.right = 0
+      outer.top = 0
 
       [mode.main.binding]
+      alt-h = 'focus left'
       alt-j = 'focus down'
       alt-k = 'focus up'
       alt-l = 'focus right'
-      alt-h = 'focus left'
 
+      alt-shift-b = 'balance-sizes'
+      alt-shift-f = 'flatten-workspace-tree'
+
+      alt-shift-h = 'move left'
       alt-shift-j = 'move down'
       alt-shift-k = 'move up'
       alt-shift-l = 'move right'
-      alt-shift-h = 'move left'
 
-      #alt-h = 'split horizontal'
-      #alt-v = 'split vertical'
+      alt-shift-i = 'join-with up'
+      alt-shift-o = 'join-with right'
+      alt-shift-u = 'join-with down'
+      alt-shift-y = 'join-with left'
+
+      alt-comma = 'layout accordion horizontal vertical'
+      alt-slash = 'layout tiles horizontal vertical'
 
       alt-f = 'fullscreen'
 
-      #alt-d = 'layout v_accordion' # 'layout stacking' in i3
-      alt-d = 'layout h_accordion tiles' # 'layout tabbed' in i3
-      #alt-e = 'layout tiles horizontal vertical' # 'layout toggle split' in i3
-
       alt-shift-space = 'layout floating tiling' # 'floating toggle' in i3
 
-      alt-q = 'workspace 1'
-      alt-w = 'workspace 2'
-      alt-e = 'workspace 3'
-      alt-r = 'workspace 4'
-      alt-t = 'workspace 5'
+      alt-1 = 'workspace 1'
+      alt-2 = 'workspace 2'
+      alt-3 = 'workspace 3'
+      alt-4 = 'workspace 4'
+      alt-5 = 'workspace 5'
+      alt-6 = 'workspace 6'
+      alt-7 = 'workspace 7'
+      alt-8 = 'workspace 8'
+      alt-9 = 'workspace 9'
 
-      alt-shift-q = 'move-node-to-workspace 1'
-      alt-shift-w = 'move-node-to-workspace 2'
-      alt-shift-e = 'move-node-to-workspace 3'
-      alt-shift-r = 'move-node-to-workspace 4'
-      alt-shift-t = 'move-node-to-workspace 5'
+      alt-shift-1 = 'move-node-to-workspace 1'
+      alt-shift-2 = 'move-node-to-workspace 2'
+      alt-shift-3 = 'move-node-to-workspace 3'
+      alt-shift-4 = 'move-node-to-workspace 4'
+      alt-shift-5 = 'move-node-to-workspace 5'
+      alt-shift-6 = 'move-node-to-workspace 6'
+      alt-shift-7 = 'move-node-to-workspace 7'
+      alt-shift-8 = 'move-node-to-workspace 8'
+      alt-shift-9 = 'move-node-to-workspace 9'
+
+      # See: https://nikitabobko.github.io/AeroSpace/commands#workspace-back-and-forth
+      alt-tab = 'workspace-back-and-forth'
+      # See: https://nikitabobko.github.io/AeroSpace/commands#move-workspace-to-monitor
+      alt-shift-tab = 'move-workspace-to-monitor --wrap-around next'
 
       alt-shift-c = 'reload-config'
 
+      alt-shift-equal = 'resize smart +50'
+      alt-shift-minus = 'resize smart -50'
+
       [workspace-to-monitor-force-assignment]
+      0 = 'main'
       1 = 'main'
       2 = 'main'
       3 = 'main'
       4 = 'main'
       5 = ['built-in', 'secondary', 'main']
+      6 = 'main'
+      7 = 'main'
+      8 = 'main'
+      9 = 'main'
 
       # todo
       [[on-window-detected]]
@@ -116,6 +142,10 @@
       if.app-name-regex-substring = '1password'
       run = 'move-node-to-workspace 4'
 
+      [[on-window-detected]]
+      if.app-name-regex-substring = 'yubico'
+      run = 'move-node-to-workspace 4'
+
       # business communication
       [[on-window-detected]]
       if.app-name-regex-substring = 'zoom'
@@ -124,6 +154,15 @@
       [[on-window-detected]]
       if.app-name-regex-substring = 'slack'
       run = 'move-node-to-workspace 5'
+
+      # design
+      [[on-window-detected]]
+      if.app-name-regex-substring = 'figma'
+      run = 'move-node-to-workspace 6'
+
+      [[on-window-detected]]
+      if.app-name-regex-substring = 'whimsical'
+      run = 'move-node-to-workspace 6'
     '';
   };
 }
